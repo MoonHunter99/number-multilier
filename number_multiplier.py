c218bc7d584520cb805267d68f6c1c1ec8d82086
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-25 15:53:09 +0800

    Pseudocode

---
+++ number_multiplier.py
@@ -0,0 +1,21 @@
+#open the needed files
+
+    #for loops to read the lines
+
+        #convert the lines to integeers
+
+        #check if even
+
+            #square the number
+
+            #convert to sring
+
+            #write to even file
+
+        #check if odd
+
+            #cube the number
+
+            #convert to sring
+
+            #write to even file

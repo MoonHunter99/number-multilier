b515fae2cf451a81e23c3cd3c0a7494b7214554a
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-25 15:58:16 +0800

    converted lines to integers

--- number_multiplier.py
+++ number_multiplier.py
@@ -1,9 +1,9 @@
 #open the needed files
 with open("integers.txt" , "r") as integers_file, open("double.txt" , "a") as double_file, open("tripple.txt" , "a") as tripple_file:
     #for loops to read the lines
-
+    for line in integers_file:
         #convert the lines to integeers
-
+        number = int(line)
         #check if even
 
             #square the number
